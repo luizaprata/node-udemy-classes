b6062e109255bd0c670fd9aeaf143717d7480a06
Luiza Prata <luiza.passos@lambda3.com.br>
2019-08-20 18:40:01 -0300

    adicionando comandos por yargs

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,10 +1,44 @@
 const validator = require("validator");
-
 const chalk = require("chalk");
+const yargs = require("yargs");
 
-console.log(`
-  ${chalk.blue.inverse.bold("validator")} ${chalk.red(
-  validator.isEmail("luiza.prata@gmail")
-)}`);
+yargs.command({
+  command: "add",
+  describe: chalk.blue.bold("adding a new note"),
+  builder: {
+    title: {
+      describe: "adding note title",
+      demandOption: true,
+      type: "string"
+    }
+  },
+  handler: function(args) {
+    console.log("adding", args.title);
+  }
+});
 
-console.log(process.argv);
+yargs.command({
+  command: "remove",
+  describe: chalk.blue.bold("removing a new note"),
+  handler: function() {
+    console.log("remove");
+  }
+});
+
+yargs.command({
+  command: "list",
+  describe: chalk.blue.bold("list all notes"),
+  handler: function() {
+    console.log("list");
+  }
+});
+
+yargs.command({
+  command: "read",
+  describe: chalk.blue.bold("read a note"),
+  handler: function() {
+    console.log("read");
+  }
+});
+
+yargs.parse();
